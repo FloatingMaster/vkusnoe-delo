e76d54e48d499319a4a33552b396a48e7dc13f8a
FlyingLeafe <flyingleafe@gmail.com>
2012-10-28 01:54:56 +0900

    try to use capistrano deployment

---
+++ application/config/less.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'directories' => array(
+        path('app').'less' => path('public').'css'
+    ),
+    'files' => array(
+
+    ),
+    'snippets' => array(
+
+    ),
+);
--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,1 +1,19 @@
-Hello, man!
+<!doctype html>
+<html>
+<head>
+	<title>
+		@section('title')
+			Вкусное Дело | Социальная сеть для кулинаров
+		@yield_section
+	</title>
+	{{ Asset::styles();  }}
+	{{ Asset::scripts(); }}
+</head>
+<body>
+	@section('main')
+	<div class='container'>
+		Test
+	</div>
+	@yield_section
+</body>
+</html>
